fc7134bdd9ef71100a843ce059a4ccaa11744ce9
Monique Exaltação <85373954+nickexaltacao@users.noreply.github.com>
2021-07-04 12:20:19 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,85 @@
+//function clicou(){
+//document.getElementById("Agradecimento");
+//console.log(document.getElementById("Agradecimento"));
+    //alert("Obrigado por Clicar");
+}
+
+
+/*function soma(n1, n2){
+    return n1 + n2;
+
+
+
+    function validaIdade(idade){
+        var validar;
+        if (idade>=18)}
+        validar = true
+    }else{
+        validar=false
+    }
+    return validar;
+    }
+
+    var idade= prompt("Qual sua idade");
+    console.log(validaIdade(idade));
+//
+//alert (setReplace("Vai Japão", "Japão", "Brasil"));
+
+/*var d = new Date();
+alert(d);
+
+
+/*var count;
+for (count=0; count <= 5; count++){
+    alert(count);
+};
+
+
+/*var count = 0;
+while (count < 5){
+    console.log(count);
+    count = count + 1;
+};
+
+
+
+
+/*var idade = prompt("Qual a sua idade?");
+//var idade = 18;
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}
+
+
+//var fruta = {nome: "maçã", cor: "vermelha"}
+//console.log(fruta.nome);
+//alert(fruta.cor);
+
+//var lista = ["maçã", "pêra", "laranja"];
+//console.log(lista);
+//lista.push("uva");
+//lista.pop();
+//console.log(lista);
+//console.log(lista.length);
+
+
+//console.log([1]);
+//alert(lista[1]);
+
+
+
+/*
+var nome = "Monique";
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo";
+//alert(nome +  "tem" + idade +  "anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1*n2);
+//console.log(frase.replace("Japão","Brasil"));
+//alert(frase.replace("Japão","Brasil"));
+console.log(frase.toUpperCase());
+*/
